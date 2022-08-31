c9be94d4a90048301eb61a50a89d19c59a082cd6
henrique97-hub <h.godoy97@hotmail.com>
2022-08-31 20:36:34 -0300

    Implementação da camera no aplicativo

--- app_car/lib/main.dart
+++ app_car/lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:app_car/ui/pagina_configuracoes.dart';
 import 'package:app_car/ui/pagina_cadastro.dart';
+import 'package:app_car/videocamp.dart';
 import 'package:app_car/widgets/botoes_rodape.dart';
 import 'package:flutter/material.dart';
 
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: RegisterPage(),
+    return const MaterialApp(
+      // home: RegisterPage(),
+      home: VideoCam(),
     );
   }
 }
-
