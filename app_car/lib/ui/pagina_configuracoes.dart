248da5cf108732f3b3021770f00e86ad44e64212
Rankannko <azevedomasterjp27@hotmail.com>
2022-09-07 10:35:41 -0300

    Commit Inicial Integração MQTT

--- app_car/lib/ui/pagina_configuracoes.dart
+++ app_car/lib/ui/pagina_configuracoes.dart
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, avoid_print
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-=======
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'dart:async';
-import 'dart:convert';
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
 import 'package:app_car/ui/camera_externa.dart';
 import 'package:app_car/ui/camera_interna.dart';
 import 'package:app_car/ui/pagina_login.dart';
@@ -19,13 +12,8 @@ import 'package:app_car/widgets/botoes_rodape.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart' as mqtt;
-<<<<<<< HEAD
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
-
-=======
-import 'package:mqtt_client/mqtt_server_client.dart';
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -36,9 +24,8 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
-<<<<<<< HEAD
   @override
-  String broker = '192.168.165.10';
+  String broker = '201.81.74.83';
   int port = 1883;
   String clientIdentifier = 'android-jp';
   String topic = 'esp32Sensor/status';
@@ -46,6 +33,7 @@ class _HomePageState extends State<HomePage> {
   String topic3 = 'esp32Sensor/S/presenca';
   String topic4 = 'esp32Sensor/S/movimento';
   String topic5 = 'esp32Sensor/S/GPS';
+  String topic6 = 'esp32Sensor/comunicacao';
   
   mqtt.MqttClient? client;
   mqtt.MqttConnectionState? connectionState;
@@ -56,6 +44,7 @@ class _HomePageState extends State<HomePage> {
   StreamSubscription? subscription3;
   StreamSubscription? subscription4;
   StreamSubscription? subscription5;
+  StreamSubscription? subscription6;
 
   @override
   void initState() {
@@ -78,45 +67,6 @@ class _HomePageState extends State<HomePage> {
   String _GPS =''; 
 
 
-=======
-  // @override
-  // String broker = '201.81.74.83';
-  // int port = 1503;
-  // String clientIdentifier = 'android-jp';
-  // String topic = 'esp32Sensor/status';
-  // String topic2 = 'esp32Sensor/alarme';
-  // String topic3 = 'esp32Sensor/S/Presencia';
-  // String topic4 = 'esp32Sensor/S/movimento';
-  // String topic5 = 'esp32Sensor/S/GPS';
-
-  // late mqtt.MqttClient client;
-  // late mqtt.MqttConnectionState connectionState;
-
-  // late StreamSubscription subscription1;
-  // late StreamSubscription subscription2;
-  // late StreamSubscription subscription3;
-  // late StreamSubscription subscription4;
-  // late StreamSubscription subscription5;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   WidgetsBinding.instance!.addPostFrameCallback((_) => _connect());
-  // }
-
-  // void _subscribeToTopic(String topic) {
-  //   if (connectionState == mqtt.MqttConnectionState.connected) {
-  //     print('[MQTT client] Subscribing to ${topic.trim()}');
-  //     client.subscribe(topic, mqtt.MqttQos.exactlyOnce);
-  //   }
-  // }
-
-  // bool _pres = false;
-  // bool _buzz = false;
-  // bool _mov = false;
-  // bool _conect = false;
-  // String _GPS = '';
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
 
   Widget build(BuildContext context) {
     final ThemeData tema = ThemeData(brightness: Brightness.dark);
@@ -154,7 +104,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           SizedBox(height: 5),
-<<<<<<< HEAD
           // Row(
           //   children: [
           //     Expanded(
@@ -166,20 +115,6 @@ class _HomePageState extends State<HomePage> {
           //     ),
           //   ],
           // ),
-=======
-          Row(
-            children: [
-              Expanded(
-                child: SizedBox(
-                  height: 200,
-                    child: const Image(
-                      image: AssetImage('images/carApp.png'),
-                    ),
-                    ),
-              ),
-            ],
-          ),
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
           SizedBox(height: 5),
           Container(
             alignment: Alignment.bottomLeft,
@@ -191,17 +126,6 @@ class _HomePageState extends State<HomePage> {
           SizedBox(height: 20),
           Card(
             color: Colors.grey[800],
-<<<<<<< HEAD
-            child: Expanded(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.battery_charging_full_rounded,
-                      color: Colors.white,
-                    ),
-=======
             child: Row(
               children: [
                 Expanded(
@@ -220,7 +144,6 @@ class _HomePageState extends State<HomePage> {
                         style: TextStyle(color: Colors.white),
                       ),
                     ],
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
                   ),
                 ),
               ],
@@ -231,37 +154,15 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-<<<<<<< HEAD
-                if (_pres == true)...[
-                  BotaoAlerta(
-                  Icon(Icons.campaign, color: Colors.red),
-                ),
-                ]else ...[
-                  BotaoAlerta(
-                  Icon(Icons.campaign, color: Colors.white),
-=======
                 BotaoAlerta(
                   Icon(Icons.campaign, color: Colors.white),'campainha'
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
                 ),
-                ],
                 BotaoAlerta(
                   Icon(Icons.phone, color: Colors.white), 'telefone'
                 ),
-<<<<<<< HEAD
-                if (_pres == true)...[
-                  BotaoAlerta(
-                  Icon(Icons.album, color: Colors.red),
-                ),
-                ]else ...[
-                  BotaoAlerta(
-                  Icon(Icons.album, color: Colors.white),
-=======
                 BotaoAlerta(
                   Icon(Icons.album, color: Colors.white), 'gravação'
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
                 ),
-                ],
                 BotaoAlerta(
                   Icon(Icons.map, color: Colors.white), 'GPS'
                 ),
@@ -306,14 +207,13 @@ class _HomePageState extends State<HomePage> {
       // bottomNavigationBar: BottomNavigation(),
     );
   }
-<<<<<<< HEAD
   
   int _counter = 0;
   void _incrementCounter() {
     setState(() {
       _counter++;
       _publishMessage(
-          topic2, "true");
+          topic6, "Essa mensagem está sendo enviada do aplicativo flutter para o tópico comunicacao");
     });
   }
 
@@ -360,6 +260,8 @@ class _HomePageState extends State<HomePage> {
     _subscribeToTopic(topic4);
     subscription5 = client?.updates?.listen(_onMessageGPS);
     _subscribeToTopic(topic5);
+    subscription6= client?.updates?.listen(_onMessageGPS);
+    _subscribeToTopic(topic6);
   }
 
   /*
@@ -461,152 +363,7 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       _GPS = String.fromEnvironment(message);
     });
+    
   }
 
 }
-=======
-
-  // void _connect() async {
-  //   client = MqttServerClient(broker, clientIdentifier);
-  //   client.port = port;
-  //   client.keepAlivePeriod = 30;
-  //   client.onDisconnected = _onDisconnected;
-
-  //   final mqtt.MqttConnectMessage connMess = mqtt.MqttConnectMessage()
-  //       .withClientIdentifier(clientIdentifier)
-  //       .startClean() // Non persistent session for testing
-  //       .keepAliveFor(30)
-  //       .withWillQos(mqtt.MqttQos.atMostOnce);
-  //   print('[MQTT client] MQTT client connecting....');
-  //   client.connectionMessage = connMess;
-
-  //   try {
-  //     await client.connect();
-  //   } catch (e) {
-  //     print("erro");
-  //     _disconnect();
-  //   }
-
-  //   /// Check if we are connected
-  //   if (client.connectionState == mqtt.MqttConnectionState.connected) {
-  //     print('[MQTT client] connected');
-  //     setState(() {
-  //       connectionState = client.connectionState!;
-  //     });
-  //   } else {
-  //     print('[MQTT client] ERROR: MQTT client connection failed - '
-  //         'disconnecting, state is ${client.connectionState}');
-  //     _disconnect();
-  //   }
-
-  //   subscription1 = client.updates!.listen(_onMessageConect);
-  //   _subscribeToTopic(topic);
-  //   subscription2 = client.updates!.listen(_onMessagePres);
-  //   _subscribeToTopic(topic2);
-  //   subscription3 = client.updates!.listen(_onMessageBuzz);
-  //   _subscribeToTopic(topic3);
-  //   subscription4 = client.updates!.listen(_onMessageMov);
-  //   _subscribeToTopic(topic4);
-  //   subscription5 = client.updates!.listen(_onMessageGPS);
-  //   _subscribeToTopic(topic5);
-  // }
-
-  // void _disconnect() {
-  //   print('[MQTT client] _disconnect()');
-  //   client.disconnect();
-  //   _onDisconnected();
-  // }
-
-  // void _onDisconnected() {
-  //   print('[MQTT client] _onDisconnected');
-  //   setState(() {
-  //     //topics.clear();
-  //     connectionState = client.connectionState!;
-  //     client = 0 as mqtt.MqttClient;
-  //     subscription1.cancel();
-  //     subscription1 = 0 as StreamSubscription;
-  //   });
-  //   print('[MQTT client] MQTT client disconnected');
-  // }
-
-  // void _onMessageConect(List<mqtt.MqttReceivedMessage> event) {
-  //   print(event.length);
-  //   final mqtt.MqttPublishMessage recMess =
-  //       event[0].payload as mqtt.MqttPublishMessage;
-  //   final String message =
-  //       mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-  //   print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
-  //       'payload is <-- ${message} -->');
-  //   print(client.connectionState);
-  //   print("[MQTT client] message with topic: ${event[0].topic}");
-  //   print("[MQTT client] message with message: ${message}");
-  //   setState(() {
-  //     _conect = bool.fromEnvironment(message);
-  //   });
-  // }
-
-  // void _onMessagePres(List<mqtt.MqttReceivedMessage> event) {
-  //   print(event.length);
-  //   final mqtt.MqttPublishMessage recMess =
-  //       event[0].payload as mqtt.MqttPublishMessage;
-  //   final String message =
-  //       mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-  //   print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
-  //       'payload is <-- ${message} -->');
-  //   print(client.connectionState);
-  //   print("[MQTT client] message with topic: ${event[0].topic}");
-  //   print("[MQTT client] message with message: ${message}");
-  //   setState(() {
-  //     _pres = bool.fromEnvironment(message);
-  //   });
-  // }
-
-  // void _onMessageBuzz(List<mqtt.MqttReceivedMessage> event) {
-  //   print(event.length);
-  //   final mqtt.MqttPublishMessage recMess =
-  //       event[0].payload as mqtt.MqttPublishMessage;
-  //   final String message =
-  //       mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-  //   print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
-  //       'payload is <-- ${message} -->');
-  //   print(client.connectionState);
-  //   print("[MQTT client] message with topic: ${event[0].topic}");
-  //   print("[MQTT client] message with message: ${message}");
-  //   setState(() {
-  //     _buzz = bool.fromEnvironment(message);
-  //   });
-  // }
-
-  // void _onMessageMov(List<mqtt.MqttReceivedMessage> event) {
-  //   print(event.length);
-  //   final mqtt.MqttPublishMessage recMess =
-  //       event[0].payload as mqtt.MqttPublishMessage;
-  //   final String message =
-  //       mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-  //   print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
-  //       'payload is <-- ${message} -->');
-  //   print(client.connectionState);
-  //   print("[MQTT client] message with topic: ${event[0].topic}");
-  //   print("[MQTT client] message with message: ${message}");
-  //   setState(() {
-  //     _mov = bool.fromEnvironment(message);
-  //   });
-  // }
-
-  // void _onMessageGPS(List<mqtt.MqttReceivedMessage> event) {
-  //   print(event.length);
-  //   final mqtt.MqttPublishMessage recMess =
-  //       event[0].payload as mqtt.MqttPublishMessage;
-  //   final String message =
-  //       mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-  //   print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
-  //       'payload is <-- ${message} -->');
-  //   print(client.connectionState);
-  //   print("[MQTT client] message with topic: ${event[0].topic}");
-  //   print("[MQTT client] message with message: ${message}");
-  //   setState(() {
-  //     _GPS = String.fromEnvironment(message);
-  //   });
-  // }
-}
->>>>>>> 0ce281785b960c8c3816a70ca488ea191697cb9b
