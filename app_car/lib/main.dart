027a4f4fb44bf3d48fe5d1ed6832016d67ea3bbb
henrique97-hub <h.godoy97@hotmail.com>
2022-09-07 11:31:52 -0300

    inicio implementação firebase

--- app_car/lib/main.dart
+++ app_car/lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:app_car/services/auth_service.dart';
 import 'package:app_car/ui/pagina_configuracoes.dart';
 import 'package:app_car/ui/pagina_cadastro.dart';
 import 'package:app_car/videocamp.dart';
 import 'package:app_car/widgets/botoes_rodape.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Firebase.initializeApp();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthService(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
